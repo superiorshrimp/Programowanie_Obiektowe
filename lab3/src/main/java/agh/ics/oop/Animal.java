6c609038ae72922be36ea478ffafe6bef96fdceb
superiorshrimp <szymon.zychowicz300701@gmail.com>
2021-10-28 20:55:55 +0200

    l3

--- lab3/src/main/java/agh/ics/oop/Animal.java
+++ lab3/src/main/java/agh/ics/oop/Animal.java
@@ -7,22 +7,33 @@ public class Animal{
         this.facing = MapDirection.NORTH;
         this.position = new Vector2d(2,2);
     }
+    public MapDirection getFacing(){return this.facing;}
+    public Vector2d getPosition(){return this.position;}
+    public boolean isAt(Vector2d loc){return this.position.equals(loc);}
     public String toString(){
         return this.facing.toString() + this.position.toString();
     }
     public void move(MoveDirection direction){
         switch (direction) {
-            case LEFT -> this.facing = this.facing.previous();
-            case RIGHT -> this.facing = this.facing.next();
-            case FORWARD -> {
+            case LEFT: {
+                this.facing = this.facing.previous();
+                break;
+            }
+            case RIGHT: {
+                this.facing = this.facing.next();
+                break;
+            }
+            case FORWARD: {
                 if (this.position.add(this.facing.toUnitVector()).precedes(new Vector2d(4, 4)) && this.position.add(this.facing.toUnitVector()).follows(new Vector2d(0, 0))) {
                     this.position = this.position.add(this.facing.toUnitVector());
                 }
+                break;
             }
-            case BACKWARD -> {
+            case BACKWARD: {
                 if (this.position.subtract(this.facing.toUnitVector()).precedes(new Vector2d(4, 4)) && this.position.subtract(this.facing.toUnitVector()).follows(new Vector2d(0, 0))) {
                     this.position = this.position.subtract(this.facing.toUnitVector());
                 }
+                break;
             }
         }
     }
